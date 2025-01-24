10f96aeecb43c6593a7639a9216e97c70044a7ba
Suyash Sumaroo <suyash@codevigor.com>
2025-01-24 18:15:37 -0500

    bug corrections for the backup file

--- BACKUP-FILE.cbl
+++ BACKUP-FILE.cbl
@@ -22,10 +22,10 @@
        01  FILE-STATUS            PIC XX.
            88  FILE-OK             VALUE '00'.
            88  FILE-ERROR          VALUE '10'.
-       01  BACKUP-ID              PIC 9(5).
-       01  FILE-NAME              PIC A(30).
-       01  BACKUP-DATE            PIC 9(8).
-       01  BACKUP-DATA            PIC A(100).
+       01  WS-BACKUP-ID           PIC 9(5).
+       01  WS-FILE-NAME           PIC A(30).
+       01  WS-BACKUP-DATE         PIC 9(8).
+       01  WS-BACKUP-DATA         PIC A(100).
 
        PROCEDURE DIVISION.
 
@@ -36,17 +36,21 @@
            END-IF
 
        DISPLAY 'Enter Backup ID: '
-       ACCEPT BACKUP-ID
+       ACCEPT WS-BACKUP-ID
        DISPLAY 'Enter File Name: '
-       ACCEPT FILE-NAME
+       ACCEPT WS-FILE-NAME
        DISPLAY 'Enter Backup Date (YYYYMMDD): '
-       ACCEPT BACKUP-DATE
+       ACCEPT WS-BACKUP-DATE
        DISPLAY 'Enter Backup Data: '
-       ACCEPT BACKUP-DATA
+       ACCEPT WS-BACKUP-DATA
+
+       MOVE WS-BACKUP-ID TO BACKUP-ID IN BACKUP-REC
+       MOVE WS-FILE-NAME TO FILE-NAME IN BACKUP-REC
+       MOVE WS-BACKUP-DATE TO BACKUP-DATE IN BACKUP-REC
+       MOVE WS-BACKUP-DATA TO BACKUP-DATA IN BACKUP-REC
 
        WRITE BACKUP-REC
 
        CLOSE BACKUP-FILE
        DISPLAY 'Backup Record Added Successfully.'
        STOP RUN.
-
